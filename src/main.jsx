f57da25b4d8034511da4da4a5a7164368a110672
Lokeshkarri231 <kv0246@srmist.edu.in>
2026-03-17 18:40:53 +0530

    Fix main.jsx routing crash

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import App from "./App.jsx";
 import "./index.css";
-
 import { BrowserRouter } from "react-router-dom";
 import { Toaster } from "react-hot-toast";
 
@@ -10,7 +9,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
       <App />
-      <Toaster position="top-right" />
+      <Toaster />
     </BrowserRouter>
   </React.StrictMode>
 );
