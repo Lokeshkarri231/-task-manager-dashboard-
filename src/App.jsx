d03b263dd24ff3161d2a558378f6ab97180b3738
Lokesh Karri <kv0246@srmist.edu.in>
2026-03-31 11:47:57 +0530

    fix profile route rendering issue

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,17 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import Dashboard from "./pages/Dashboard";
 import Profile from "./pages/Profile";
 
-
 function App() {
   return (
-    <BrowserRouter>
-      import Profile from "./pages/Profile";
-
-<Routes>
-  <Route path="/" element={<Login />} />
-  <Route path="/dashboard" element={<Dashboard />} />
-  <Route path="/profile" element={<Profile />} />
-</Routes>
-    </BrowserRouter>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+    </Router>
   );
 }
 
